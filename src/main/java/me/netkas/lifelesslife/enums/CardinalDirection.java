f19fa5d955c76f7c7b9ba8c4fb103695f2bf0c4c
netkas <netkas@n64.cc>
2024-10-07 15:06:48 -0400

    Add CardinalDirection enum with utility methods

---
+++ src/main/java/me/netkas/lifelesslife/enums/CardinalDirection.java
@@ -0,0 +1,111 @@
+package me.netkas.lifelesslife.enums;
+
+import java.util.Random;
+
+public enum CardinalDirection
+{
+    /**
+     * Represents the cardinal direction "north".
+     */
+    NORTH("north"),
+
+    /**
+     * Represents the cardinal direction "east".
+     */
+    EAST("east"),
+
+    /**
+     * Represents the cardinal direction "south".
+     */
+    SOUTH("south"),
+
+    /**
+     * Represents the cardinal direction "west".
+     */
+    WEST("west");
+
+    private final String name;
+
+    /**
+     * Represents a cardinal direction (north, east, south, west).
+     */
+    CardinalDirection(String name)
+    {
+        this.name = name;
+    }
+
+    /**
+     * Returns the name associated with the CardinalDirection.
+     *
+     * @return the name associated with the CardinalDirection
+     */
+    public String getName()
+    {
+        return name;
+    }
+
+    /**
+     * Generates a random CardinalDirection enum value.
+     *
+     * @return a random CardinalDirection enum value
+     */
+    public static CardinalDirection random()
+    {
+        return values()[(int) (Math.random() * values().length)];
+    }
+
+    /**
+     * Generates a random CardinalDirection enum value.
+     *
+     * @param random the RandomGenerator object used to generate the random value
+     * @return a random CardinalDirection enum value
+     */
+    public static CardinalDirection random(Random random)
+    {
+        if (random == null)
+        {
+            return random();
+        }
+
+        return values()[random.nextInt(values().length)];
+    }
+
+    /**
+     * Returns the cardinal direction that is to the left of the current cardinal direction.
+     *
+     * @return the cardinal direction to the left
+     */
+    public CardinalDirection getLeft()
+    {
+        return switch (this)
+        {
+            case NORTH -> WEST;
+            case EAST -> NORTH;
+            case SOUTH -> EAST;
+            case WEST -> SOUTH;
+        };
+    }
+
+    /**
+     * Returns the cardinal direction that is to the right of the current cardinal direction.
+     * The right direction is determined based on the current cardinal direction as follows:
+     *
+     * - If the current cardinal direction is NORTH, the right direction is EAST.
+     * - If the current cardinal direction is EAST, the right direction is SOUTH.
+     * - If the current cardinal direction is SOUTH, the right direction is WEST.
+     * - If the current cardinal direction is WEST, the right direction is NORTH.
+     *
+     * @return the cardinal direction that is to the right of the current cardinal direction
+     */
+    public CardinalDirection getRight()
+    {
+        return switch (this)
+        {
+            case NORTH -> EAST;
+            case EAST -> SOUTH;
+            case SOUTH -> WEST;
+            case WEST -> NORTH;
+        };
+    }
+
+}
